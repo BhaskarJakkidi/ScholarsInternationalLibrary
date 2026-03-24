3f62b14e1461ae76cea687cd0760364cc87aff1e
bhaskarj6 <bhaskarreddy.j@gmail.com>
2026-03-24 23:26:24 +0530

    Update test_query.py

--- test_query.py
+++ test_query.py
@@ -1,10 +1,13 @@
 import duckdb
-
+DB_NAME = "mnt/data/library.duckdb"
+def connect_db():
+    # Persistent DuckDB file
+    return duckdb.connect(DB_NAME)
 # Connect to your DuckDB file
-conn = duckdb.connect("library.duckdb")
+conn = connect_db()
 
 # Run a query
-rows = conn.execute("select * FROM users").fetchall()
+rows = conn.execute("select *  FROM users").fetchall()
 
 for row in rows:
     print(row)
